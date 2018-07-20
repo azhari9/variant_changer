4223d34b4ff72dbddb80a2295f86a52e0b6052af
muhd_azhari <muhd_azhari@clarion.com.my>
2018-07-20 09:47:16 +0800

    Add build output binary

---
+++ build/CMakeFiles/VariantChanger.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VariantChanger.dir/variant_changer.c.o"
+  "CMakeFiles/VariantChanger.dir/sw_update.c.o"
+  "VariantChanger.pdb"
+  "VariantChanger"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/VariantChanger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
